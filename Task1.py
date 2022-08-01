9cbdf5510cab16da7c6476d5f3ee4a3b0384c3a0
marombracha <73559031+marombracha@users.noreply.github.com>
2022-08-01 14:22:14 +0300

    Create Task1

---
+++ Task1.py
@@ -0,0 +1,35 @@
+import csv
+from datetime import datetime, timedelta
+
+with open('Flights.csv', 'r') as file:
+    reader = csv.DictReader(file, skipinitialspace=True)
+
+    flights_list = list(reader)
+
+print(flights_list)
+
+flights_list.sort(key=lambda flight_time: datetime.strptime(flight_time['Arrival'][:-1], '%H:%M').time())
+
+print(flights_list)
+
+success_counter = 0
+
+for flight in flights_list:
+    time_change = datetime.strptime(flight['Departure '][:-1], '%H:%M') - datetime.strptime(flight['Arrival'] , '%H:%M')
+    if success_counter < 20 and time_change > timedelta(hours=3):
+        flight['success'] = True
+    else:
+        flight['success'] = False
+
+
+headers = flights_list[0].keys()
+
+with open('Flights-readysorted.csv', 'w') as file:
+    dict_write = csv.DictWriter(file, headers)
+    dict_write.writeheader()
+    dict_write.writerows(flights_list)
+
+
+
+
+
